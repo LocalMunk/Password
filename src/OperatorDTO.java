85351f3b9690332282ae36bdf3ca89261fdc090e
Martin Dalby <Martin Dalby@10.16.167.29>
2016-02-16 10:32:14 +0100

    Added method to validate a password, and begun work on password generator

--- src/OperatorDTO.java
+++ src/OperatorDTO.java
@@ -65,4 +65,103 @@ public class OperatorDTO {
 	public void setOprId(int oprId) {
 		this.oprId = oprId;
 	}
+	
+	public String generatePassword(){
+		String password = "";
+		while(passwordValidation(password) == false){
+			int j = (int)(Math.random() * 4 + 1);
+			
+		}
+		return password;
+	}
+	
+	public boolean passwordValidation(String password){
+		boolean out;
+		int checks = 0;
+		if(password.length() <= 5){
+			return false;
+		}
+		if(checkLetters(password)){
+			checks++;
+		}
+		if(checkCapitals(password)){
+			checks++;
+		}
+		
+		if(checkNumbers(password)){
+			checks++;
+		}
+		
+		if(checkSymbols(password)){
+			checks++;
+		}
+		
+		if(checks  >= 3){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean checkLetters(String x){
+		boolean loop = true;
+		int i = 97;
+		int j;
+		while(loop){
+			j = x.indexOf(i);
+			if(j >= 0){
+				return true;
+			}
+			i++;
+			if(i >= 123);		
+			}
+		return false;
+	}
+	
+	public boolean checkCapitals(String x){
+		boolean loop = true;
+		int i = 65;
+		int j;
+		while(loop){
+			j = x.indexOf(i);
+			if(j >= 0){
+				return true;
+			}
+			i++;
+			if(i >= 91);		
+			}
+		return false;
+	}
+	
+	public boolean checkNumbers(String x){
+		boolean loop = true;
+		int i = 48;
+		int j;
+		while(loop){
+			j = x.indexOf(i);
+			if(j >= 0){
+				return true;
+			}
+			i++;
+			if(i >= 58);		
+			}
+		return false;
+	}
+	
+	public boolean checkSymbols(String x){
+		boolean loop = true;
+		int i = 0;
+		int j;
+		int[] k = {46, 45, 95, 43, 33, 63, 61};
+		while(loop){
+			j = x.indexOf(k[i]);
+			if(j >= 0){
+				return true;
+			}
+			i++;
+			if(k[i] >= 7);		
+			}
+		return false;
+	}
 }
